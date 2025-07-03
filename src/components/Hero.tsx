a086398b2bd965b52c2520bf89801d61b1eedc6f
Chrestiondior77 <ishakkorichi09@gmail.com>
2025-07-03 12:29:27 +0100

    fixed error link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion'
-import { Link } from 'react-router-dom'
 import { useModal } from '../contexts/ModalContext';
 import { RegisterForm } from './RegisterForm';
 
